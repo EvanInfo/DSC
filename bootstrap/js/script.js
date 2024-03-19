116ce4416fc8fa121531f4187887d010bc448217
EvanInfo <evanzucchiatti@gmail.com>
2024-03-19 16:52:57 +0100

    Second commit

--- bootstrap/js/script.js
+++ bootstrap/js/script.js
@@ -23,33 +23,25 @@ function validateMatricule() {
     if (/^\d{6}$/.test(matriculeValue)) {
         matriculeInput.classList.remove("is-invalid");
         matriculeInput.classList.add("is-valid");
+        return true;
     } else {
         matriculeInput.classList.add("is-invalid");
         matriculeInput.classList.remove("is-valid");
+        return false;
     }
 }
 
 function validateDate() {
-    // Récupérer la valeur de la date de naissance
     var dateInput = document.getElementById("naissance").value;
-    
-    // Créer un objet Date à partir de la valeur saisie
     var dateObject = new Date(dateInput);
-    
-    // Créer un objet Date pour la date actuelle
     var currentDate = new Date();
-    
-    // Récupérer l'élément du champ de saisie de la date de naissance
     var dateInputField = document.getElementById("naissance");
 
-    // Vérifier si la date est valide, inférieure à la date actuelle et supérieure ou égale à 1900
     if (dateObject instanceof Date && !isNaN(dateObject) && dateObject < currentDate && dateObject.getFullYear() >= 1900) {
-        // La date est valide, ajouter la classe is-valid et retirer la classe is-invalid
         dateInputField.classList.remove("is-invalid");
         dateInputField.classList.add("is-valid");
         return true;
     } else {
-        // La date n'est pas valide, ajouter la classe is-invalid et retirer la classe is-valid
         dateInputField.classList.remove("is-valid");
         dateInputField.classList.add("is-invalid");
         return false;
@@ -57,62 +49,130 @@ function validateDate() {
 }
 
 function validateName(inputId) {
-    // Récupérer la valeur saisie dans le champ de saisie
     var name = document.getElementById(inputId).value;
-    
-    // Récupérer l'élément du champ de saisie
     var inputField = document.getElementById(inputId);
 
-    // Vérifier si le nom ou le prénom ont au moins une lettre et au plus 25 caractères
     if (/^[a-zA-Z]{1,25}$/.test(name)) {
-        // Le nom ou le prénom est valide, ajouter la classe is-valid et retirer la classe is-invalid
         inputField.classList.remove("is-invalid");
         inputField.classList.add("is-valid");
-        return true; // Le nom ou le prénom est valide
+        return true;
     } else {
-        // Le nom ou le prénom n'est pas valide, ajouter la classe is-invalid et retirer la classe is-valid
         inputField.classList.remove("is-valid");
         inputField.classList.add("is-invalid");
-        return false; // Le nom ou le prénom n'est pas valide
+        return false;
     }
 }
 
 function validateSexe() {
-    // Récupérer les boutons radio pour le sexe
-    var sexeFeminin = document.getElementById("sexef");
-    var sexeMasculin = document.getElementById("sexem");
+    var sexeFeminin = document.getElementById("sexeFeminin");
+    var sexeMasculin = document.getElementById("sexeMasculin");
+    var inputFeminin = document.getElementById("sexeFeminin");
+    var inputMasculin = document.getElementById("sexeMasculin");
 
-    // Récupérer l'élément <div class="form-group row">
-    var sexeFormGroup = document.getElementById("sexeFormGroup");
-
-    // Vérifier si l'un des boutons radio est sélectionné
-    if (sexeFeminin.checked || sexeMasculin.checked) {
-        // Au moins un bouton radio est sélectionné, ajouter la classe is-valid et retirer la classe is-invalid
-        sexeFormGroup.classList.remove("is-invalid");
-        sexeFormGroup.classList.add("is-valid");
-        return true; // Au moins un bouton radio est sélectionné
+    if (sexeFeminin.checked) {
+        inputFeminin.classList.remove("is-invalid");
+        inputFeminin.classList.add("is-valid");
+        inputMasculin.classList.remove("is-valid");
+        inputMasculin.classList.remove("is-invalid");
+        return true;
+    } else if (sexeMasculin.checked) {
+        inputMasculin.classList.remove("is-invalid");
+        inputMasculin.classList.add("is-valid");
+        inputFeminin.classList.remove("is-valid");
+        inputFeminin.classList.remove("is-invalid");
+        return true;
     } else {
-        // Aucun bouton radio n'est sélectionné, ajouter la classe is-invalid et retirer la classe is-valid
-        sexeFormGroup.classList.remove("is-valid");
-        sexeFormGroup.classList.add("is-invalid");
-        return false; // Aucun bouton radio est sélectionné
+        inputFeminin.classList.remove("is-valid");
+        inputFeminin.classList.add("is-invalid");
+        inputMasculin.classList.remove("is-valid");
+        inputMasculin.classList.add("is-invalid");
+        return false;
     }
 }
 
+function validateGrade(){
+    var gradeElement = document.getElementById("Grade");
+    var selectedGrade = gradeElement.value;
+
+    if (selectedGrade !== "") {
+        gradeElement.classList.remove("is-invalid");
+        gradeElement.classList.add("is-valid");
+        return true;
+    } else {
+        gradeElement.classList.remove("is-valid");
+        gradeElement.classList.add("is-invalid");
+        return false;
+    }
+}
+
+function validateTelephone() {
+    var telephoneInput = document.getElementById("tel");
+    var telephoneValue = telephoneInput.value.trim();
+    
+    if (/^\d{10}$/.test(telephoneValue)) {
+        telephoneInput.classList.remove("is-invalid");
+        telephoneInput.classList.add("is-valid");
+        return true;
+    } else {
+        telephoneInput.classList.add("is-invalid");
+        telephoneInput.classList.remove("is-valid");
+        return false;
+    }
+}
+
+function validateCaserne(){
+    var CaserneElement = document.getElementById("Caserne");
+    var selectedCaserne = CaserneElement.value;
+
+    if (selectedCaserne!== "") {
+        CaserneElement.classList.remove("is-invalid");
+        CaserneElement.classList.add("is-valid");
+        return true;
+    } else {
+        CaserneElement.classList.remove("is-valid");
+        CaserneElement.classList.add("is-invalid");
+        return false;
+    }
+}
+
+function validateType() {
+    var professionnel = document.getElementById("professionnel");
+    var volontaire = document.getElementById("volontaire");
+    var inputProfessionnel = document.getElementById("professionnel");
+    var inputVolontaire = document.getElementById("volontaire");
+
+    if (professionnel.checked) {
+        inputProfessionnel.classList.remove("is-invalid");
+        inputProfessionnel.classList.add("is-valid");
+        inputVolontaire.classList.remove("is-valid");
+        inputVolontaire.classList.remove("is-invalid");
+        return true;
+    } else if (volontaire.checked) {
+        inputVolontaire.classList.remove("is-invalid");
+        inputVolontaire.classList.add("is-valid");
+        inputProfessionnel.classList.remove("is-valid");
+        inputProfessionnel.classList.remove("is-invalid");
+        return true;
+    } else {
+        inputProfessionnel.classList.remove("is-valid");
+        inputProfessionnel.classList.add("is-invalid");
+        inputVolontaire.classList.remove("is-valid");
+        inputVolontaire.classList.add("is-invalid");
+        return false;
+    }
+}
 
 
 
 // Lier les fonctions du formulaire à l'événement oninput 
 document.getElementById("matricule").addEventListener("input", validateMatricule);
 document.getElementById("naissance").addEventListener("input", validateDate);
-document.getElementById("sexef").addEventListener("change", validateSexe);
-document.getElementById("sexem").addEventListener("change", validateSexe);
-document.getElementById("nom").addEventListener("input", function() {
-    validateName("nom");
-});
-
-document.getElementById("prenom").addEventListener("input", function() {
-    validateName("prenom");
-});
-
-
+document.getElementById("sexeFeminin").addEventListener("change", validateSexe);
+document.getElementById("sexeMasculin").addEventListener("change", validateSexe);
+document.getElementById("nom").addEventListener("input", validateName);
+document.getElementById("prenom").addEventListener("input",  validateName);
+document.getElementById("Grade").addEventListener("change",  validateGrade);
+document.getElementById("tel").addEventListener("input", validateTelephone);
+document.getElementById("Caserne").addEventListener("change",  validateCaserne);
+document.getElementById("professionnel").addEventListener("change", validateType);
+document.getElementById("volontaire").addEventListener("change", validateType);
--- pages/formulaire.php
+++ pages/formulaire.php
@@ -1,33 +1,26 @@
 <?php
-include ("../include/entete.inc.php");
+include("../include/entete.inc.php");
 
-/*if (isset($_POST['valider']))
-{
- 
+if (isset($_POST['valider'])) {
+    $matricule = htmlspecialchars($_POST['matricule'], ENT_QUOTES, 'UTF-8');
+    $naissance = htmlspecialchars($_POST['naissance'], ENT_QUOTES, 'UTF-8');
     $nom = htmlspecialchars($_POST['nom'], ENT_QUOTES, 'UTF-8');
     $prenom = htmlspecialchars($_POST['prenom'], ENT_QUOTES, 'UTF-8');
-    $mail = filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL);
-
-    $type = ($_POST['choixType'] === 'client') ? 'client' : 'Photographe';
-    $motdepasse = ($_POST['motdepasse1']);
-
-    
-    $user = new User(['Nom' => $nom, 'Prenom' => $prenom, 'Mail' => $mail, 'Mdp' => $motdepasse, 'Type' => $type]);
-    $manager->add($user);
-    header('Location: inscriptionOK.php');
+    $tel = htmlspecialchars($_POST['tel'], ENT_QUOTES, 'UTF-8');
 }
-*/
+
+
 ?>
  
 	<div class="container">
     <?php echo generationEntete("Ajout Pompier") ?>
     <div class="jumbotron">
-    <form method="post">
+    <form method="post" id="formulaire">
       <div class="form-group row">
         <div class="form-group col-md-6">
           <label for="matricule">Matricule</label>
           <input type="number" class="form-control" name="matricule" id="matricule" placeholder="Ex: 876524" oninput="validateMatricule()" required>
-
+    
           <div class="invalid-feedback">
             Le champ matricule est obligatoire
           </div>
@@ -62,18 +55,18 @@ include ("../include/entete.inc.php");
     <div class="form-group">
       <div class="row">
           <!-- Colonne des boutons radio Sexe -->
-          <div class="col-md-6" id="sexeFormGroup">
-              <label for="inputState">Sexe :</label>
+          <div class="col-md-6" >
+              <label for="inputSexe">Sexe :</label>
               <div class="form-check">
-                  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="sexef" value="féminin" onchange="validateSexe()">
-                  <label class="form-check-label" for="sexef">Féminin</label>
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="sexeFeminin" value="féminin" onchange="validateSexe()">
+                <label class="form-check-label" for="sexeFeminin">Féminin</label>
               </div>
               <div class="form-check">
-                  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="sexem" value="masculin" onchange="validateSexe()">
-                  <label class="form-check-label" for="sexem">Masculin</label>
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="sexeMasculin" value="masculin" onchange="validateSexe()">
+                <label class="form-check-label" for="sexeMasculin">Masculin</label>
               </div>
               <div class="invalid-feedback">
-                  Veuillez choisir un sexe.
+                Veuillez choisir un sexe.
               </div>
           </div>
           
@@ -81,7 +74,7 @@ include ("../include/entete.inc.php");
           <div class="col-md-6">
               <div class="form-group">
                   <label for="inputState">Grade</label>
-                  <select id="inputState" class="form-control is-invalid" required>
+                  <select id="Grade" class="form-control " oninput="validateGrade()" required>
                       <option value="" disabled selected>Choisissez un grade</option>
                       <?php
                       $listeGrade = $gradeManager->getGrade();
@@ -101,8 +94,7 @@ include ("../include/entete.inc.php");
       <div class="form-group row">
         <div class="form-group col-md-6">
           <label for="tel">Téléphone</label>
-          <input type="tel" class="form-control is-invalid" name="tel" id="tel"  required>
-          <small id="tel" class="form-text text-muted"></small>
+          <input type="number" class="form-control " name="tel" id="tel" oninput="validateTelephone()"  required>
           <div class="invalid-feedback">
             Vous devez fournir un telephone valide.
           </div>
@@ -110,7 +102,7 @@ include ("../include/entete.inc.php");
         <div class="col-md-6">
             <div class="form-group">
                 <label for="inputState">Caserne</label>
-                <select id="inputState" class="form-control is-invalid" required>
+                <select id="Caserne" class="form-control " oninput="validateCaserne()" required>
                     <option value="" disabled selected>Choisissez une caserne</option>
                     <?php
                     $listeCaserne = $caserneManager->getCaserne();
@@ -128,19 +120,20 @@ include ("../include/entete.inc.php");
       <div class="form-group row">
         <!-- Colonne des boutons radio -->
         <div class="col-md-6">
-        <label for="inputState">Type Pompier :</label>
+        <label for="inputType">Type Pompier :</label>
             <div class="form-check form-check-inline" required>
-                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="professionnel" value="professionnel">
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="professionnel" value="professionnel" onchange="validateType()">
                 <label class="form-check-label" for="professionnel">Professionnel</label>
             </div>
             <div class="form-check form-check-inline" required>
-                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="volontaire" value="volontaire">
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="volontaire" value="volontaire" onchange="validateType()">
                 <label class="form-check-label" for="volontaire">Volontaire</label>
             </div>
         </div>
       </div>
-      <input type="submit" value="Valider" class="btn btn-primary" name="valider" />
-    </form>
+      <input type="submit" value="Valider" class="btn btn-primary" name="valider" id="boutton" />
+      
+    </form>  
   </div>
 
   

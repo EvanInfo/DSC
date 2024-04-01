4bc3b17e571008651800dce4d7f820b69d9896b1
Zucchiatti <evanzucchiatti@gmail.com>
2024-04-01 21:24:22 +0200

    Optimisation PompierManager

--- classes/PompierManager.class.php
+++ classes/PompierManager.class.php
@@ -14,31 +14,25 @@ class PompierManager
         if (!$pompier->getMatricule() || !$pompier->getNom() || !$pompier->getPrenom() || !$pompier->getDateNaiss() || !$pompier->getTel() || !$pompier->getSexe() || !$pompier->getId()) {
             throw new Exception("Tous les attributs requis doivent être définis avant d'insérer dans la base de données.");
         }
-        $pompier->setMatricule($pompier->getMatricule());
-        $pompier->setNom($pompier->getNom());
-        $pompier->setPrenom($pompier->getPrenom());
-        $pompier->setDateNaiss($pompier->getDateNaiss());
-        $pompier->setTel($pompier->getTel());
-        $pompier->setSexe($pompier->getSexe());
-        $pompier->setId($pompier->getId());
-        
-
+    
         // Préparation de la requête d'insertion
         $requete = $this->_db->prepare("INSERT INTO pompier (Matricule, Nom, Prénom, DateNaiss, Tel, Sexe, id) VALUES (:matricule, :nom, :prenom, :dateNaiss, :tel, :sexe, :id)");
-
+    
         // Liaison des valeurs avec les paramètres de la requête
-        $requete->bindValue(':matricule', $pompier->getMatricule());
-        $requete->bindValue(':nom', $pompier->getNom());
-        $requete->bindValue(':prenom', $pompier->getPrenom());
-        $requete->bindValue(':dateNaiss', $pompier->getDateNaiss());
-        $requete->bindValue(':tel', $pompier->getTel());
-        $requete->bindValue(':sexe', $pompier->getSexe());
-        $requete->bindValue(':id', $pompier->getId());
-
-
+        $valeurs = array(
+            ':matricule' => $pompier->getMatricule(),
+            ':nom' => $pompier->getNom(),
+            ':prenom' => $pompier->getPrenom(),
+            ':dateNaiss' => $pompier->getDateNaiss(),
+            ':tel' => $pompier->getTel(),
+            ':sexe' => $pompier->getSexe(),
+            ':id' => $pompier->getId()
+        );
+    
         // Exécution de la requête
-        $requete->execute();
+        $requete->execute($valeurs);
     }
+    
 
     public function setDB(PDO $db)
     {

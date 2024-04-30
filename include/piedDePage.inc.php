72fbe0f0f40aee8a8b87b8dd9f68df6964e904f8
EvanInfo <evanzucchiatti@gmail.com>
2024-04-30 16:53:10 +0200

    Mise a jour pied de page

--- include/piedDePage.inc.php
+++ include/piedDePage.inc.php
@@ -3,23 +3,14 @@
       <div class="col-12 col-md">
         <small class="d-block mb-3 text-muted">:: © 2024 : DSC ::</small>
       </div>
-      <div class="col-6 col-md">
-        <h5>Liens</h5>
-        <ul class="list-unstyled text-small">
-          <li><a class="text-muted" href="#">Nos promotions</a></li>
-          <li><a class="text-muted" href="#">Les photographes</a></li>
-          <li><a class="text-muted" href="#">Notre charte qualité</a></li>
-        </ul>
-      </div>
-      <div class="col-6 col-md">
+      <div class="col-3 col-md">
         <h5>Aides</h5>
         <ul class="list-unstyled text-small">
           <li><a class="text-muted" href="#">Nous contacter</a></li>
-          <li><a class="text-muted" href="#">Tuto</a></li>
           <li><a class="text-muted" href="#">Aide en ligne</a></li>
         </ul>
       </div>
-      <div class="col-6 col-md">
+      <div class="col-3 col-md">
         <h5>Nous connaitre</h5>
         <ul class="list-unstyled text-small">
           <li><a class="text-muted" href="#">Adresse</a></li>
